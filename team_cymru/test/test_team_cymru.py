4ba3bafd0ce19654541f60d70179de5f36c31523
blacktop <blacktop@users.noreply.github.com>
2014-04-13 20:02:45 -0700

    Adding Tests

--- team_cymru/test/test_team_cymru.py
+++ team_cymru/test/test_team_cymru.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Josh Maine'
-
+from unittest import TestCase
 from team_cymru.team_cymru_api import TeamCymruApi
 
-myteam = TeamCymruApi()
-print myteam.get_cymru('039ea049f6d0f36f55ec064b3b371c46')
-print myteam.get_cymru('5e28284f9b5f9097640d58a73d38ad4c')
-print
+
+class InitTests(TestCase):
+    def test_hash_found(self):
+        myteam = TeamCymruApi()
+
+        try:
+            print myteam.get_cymru('039ea049f6d0f36f55ec064b3b371c464545454')
+        except Exception as e:
+            self.fail(e)
+
+    def test_hash_not_found(self):
+        myteam = TeamCymruApi()
+
+        try:
+            print myteam.get_cymru('5e28284f9b5f9097640d58a73d38ad4c')
+        except Exception as e:
+            self.fail(e)
